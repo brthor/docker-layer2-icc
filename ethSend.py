5e449f32d7674d15ae1cf8ccb9472b63dda60563
Bryan Thornbury <bt.mozoby@gmail.com>
2018-02-19 20:14:36 -0800

    fix typo

--- ethSend.py
+++ ethSend.py
@@ -27,6 +27,6 @@ def sendeth(src, dst, eth_type, payload, interface = "eth0"):
 if __name__ == "__main__":
   print("Sent %d-byte Ethernet packet on eth0" %
     sendeth(b"\x02\x42\xAC\x11\x00\x03",   # Put SRC_ADDR Source Address (run `ip a` in sending container)
-            b"\x02\x42\xAC\x11\x00\x02",   # Put DEST_ADDR Destination Address (run `ip a` in sending container)
+            b"\x02\x42\xAC\x11\x00\x02",   # Put DEST_ADDR Destination Address (run `ip a` in listening container)
             b"\x06\x00",                   # Raw Ethernet 2 Frame Type
             b"hello"))
